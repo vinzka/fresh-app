59050c673362b7735fc5b646ef81abda3b83cfcb
Vincent Katz <vinz.k@live.fr>
2020-10-07 15:11:24 +0300

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'pages#home'
+
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
+  resources :activities do
+    resources :players, only: :create
+    resources :chatrooms, only: [:show] do
+      resources :messages, only: :create
+    end
+  end
+
+  resources :chatrooms, only: [:index]
+
+  get 'users/me', to: 'users#me', as: :me
+
+  resources :users, only: %i[show edit update]
+
+  resources :players, only: :destroy
 end
